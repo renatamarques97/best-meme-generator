687484c4b8f9d6c42d524adb7e8bef69c37d2ebb
Renata Marques <renata.nrmarques@gmail.com>
2020-03-09 20:26:44 -0300

    app js including main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import MemeGenerator from './components/MemeGenerator'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Best Meme Generator</h1>
+        <MemeGenerator />
       </header>
     </div>
   );
