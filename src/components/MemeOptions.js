f205bb36e2a6fed4ffdbb6d5cfd0c4a5856d5ca1
Renata Marques <renata.nrmarques@gmail.com>
2020-03-09 22:55:42 -0300

    change text by user input

--- src/components/MemeOptions.js
+++ src/components/MemeOptions.js
@@ -7,15 +7,17 @@ class MemeOptions extends Component {
     return (
       <div>
         <h3><i class="fa fa-picture-o fa-fw" aria-hidden="true"></i>Source Image</h3>
-        <h3><FontAwesomeIcon icon="coffee"/></h3>
         <p>From URL</p>
         <input id="imgURL" class="block" type="text" placeholder="Link to image" />
         <p>From Local Disk</p>
         <input id="imgFile" type="file" accept="image/*"/>
         <label for="imgFile" class="btn"><i class="fa fa-upload fa-fw"></i></label>
-        <TextField id="filled-basic" label="Link to Image" variant="filled" />
-        <TextField id="filled-basic" label="Top Text" variant="filled" />
-        <TextField id="filled-basic" label="Bottom Text" variant="filled" />
+        <input id="textTop" class="block" type="text" placeholder="Top Text" 
+          onChange={ (e) => this.props.changeTextTop(e.target.value) }
+        />
+        <input id="textBottom" class="block" type="text" placeholder="Bottom Text" 
+          onChange={ (e) => this.props.changeTextBottom(e.target.value) }
+        />
       </div>
     );
   }
