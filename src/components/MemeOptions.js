e07a9ee7bf92a80bf7e014aa6063d585326f9009
Renata Marques <renata.nrmarques@gmail.com>
2020-03-14 23:18:02 -0300

    reference link added in options

--- src/components/MemeOptions.js
+++ src/components/MemeOptions.js
@@ -7,10 +7,16 @@ class MemeOptions extends Component {
     return (
       <div>
         <h3><i class="fa fa-picture-o fa-fw" aria-hidden="true"></i>Source Image</h3>
+        {/* <h3><FontAwesomeIcon icon=""/></h3> */}
         <p>From URL</p>
-        <input id="imgURL" class="block" type="text" placeholder="Link to image" />
+        <input id="imgURL" class="block" type="text" placeholder="Link to image" 
+          onChange={ (e) => this.props.changeImage(e.target.value) }
+        />
+        <a href="https://memeful.com/" target="_blank">memeful.com</a>
         <p>From Local Disk</p>
-        <input id="imgFile" type="file" accept="image/*"/>
+        <input id="imgFile" type="file" accept="image/*"
+          onChange={ (e) => this.props.changeImage(e.target) }
+        />
         <label for="imgFile" class="btn"><i class="fa fa-upload fa-fw"></i></label>
         <input id="textTop" class="block" type="text" placeholder="Top Text" 
           onChange={ (e) => this.props.changeTextTop(e.target.value) }
