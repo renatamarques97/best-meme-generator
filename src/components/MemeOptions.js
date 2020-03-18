0d30b7eb7f7ea39bf0d20b6843b7e0b9d7e46446
Renata Marques <renata.nrmarques@gmail.com>
2020-03-18 12:41:40 -0300

    icons added

--- src/components/MemeOptions.js
+++ src/components/MemeOptions.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { TextField } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Slider from '@material-ui/core/Slider';
-import { FaDownload } from 'react-icons/fa';
-import { FaUpload } from 'react-icons/fa';
+import { FaDownload, FaUpload, FaFont } from 'react-icons/fa';
+import { AiOutlinePicture, AiOutlineFontSize } from 'react-icons/ai';
+import { GiCompactDisc } from 'react-icons/gi';
+import { TiExport } from 'react-icons/ti';
 
 class MemeOptions extends Component {
   changeImageExternal = (e) => {
@@ -19,25 +21,24 @@ class MemeOptions extends Component {
   render () { 
     return (
       <div>
-        <p>From URL</p>
-
+        <p><AiOutlinePicture /> From URL</p>
         <input id="imgURL" class="block" type="text" placeholder="Link to image" 
           onChange={ (e) => this.changeImageExternal(e) }
         />
         <a href="https://memeful.com/" target="_blank">memeful.com</a>
-        <p>From Local Disk</p>
+        <p><GiCompactDisc /> From Local Disk</p>
         <input id="imgFile" type="file" accept="image/*"
           onChange={ (e) => this.changeImageFromLocalDisk(e) }
         />
         <label for="imgFile" class="btn local-disk"><FaUpload /></label>
-        <label class=""><i class=""></i><p>Meme Text</p></label>
+        <label><p><FaFont /> Meme Text</p></label>
         <input id="textTop" class="block" type="text" placeholder="Top Text" 
           onChange={ (e) => this.props.changeTextTop(e.target.value) }
         />
         <input id="textBottom" class="block" type="text" placeholder="Bottom Text" 
           onChange={ (e) => this.props.changeTextBottom(e.target.value) }
         />
-        <label class=""><p>Text Size</p></label>
+        <label><p><AiOutlineFontSize /> Text Size</p></label>
         <label><a>Text Size Top</a></label>
         <Slider value={ this.props.textSizeTop } 
                 onChange={ this.props.changeSizeTextTop } 
@@ -52,7 +53,7 @@ class MemeOptions extends Component {
                 valueLabelDisplay="auto"
                 max={ 30 } min={ 2 }
                 />
-        <p>Export</p>
+        <p><TiExport />Export</p>
         <label class="btn local-disk"><FaDownload /></label>
       </div>      
     );
